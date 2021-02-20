5375d65c7c986a0df04193875cc3c8e68d5cb25c
陈庆敬 <c.q.j@outlook.com>
2021-02-21 00:30:19 +0800

    从配置文件中动态读取监听端口

--- src/main/java/org/heimdall/shield_server/NettyServer.java
+++ src/main/java/org/heimdall/shield_server/NettyServer.java
@@ -7,7 +7,9 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
+import java.io.FileInputStream;
 import java.net.InetSocketAddress;
+import java.util.Properties;
 
 public class NettyServer{
 
@@ -24,6 +26,9 @@ public class NettyServer{
 
     public void start() throws Exception {
         Integer port = 9527;
+        Properties properties = new Properties();
+        properties.load(new FileInputStream("D:\\shield_server\\src\\main\\resources\\application.properties"));
+        port = Integer.valueOf(properties.getProperty("server.port"));
         ServerBootstrap bootstrap = new ServerBootstrap();
         bootstrap.group(bossGroup, workGroup).channel(NioServerSocketChannel.class)
                 .localAddress(new InetSocketAddress(port))
