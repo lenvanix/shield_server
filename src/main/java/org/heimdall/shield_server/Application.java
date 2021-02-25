a126fd4a87dbc4b8b1b8ddab3bd1b3f382b9d54e
陈庆敬 <c.q.j@outlook.com>
2021-02-25 23:12:05 +0800

    kill信号捕获（准备优雅终止程序）

--- src/main/java/org/heimdall/shield_server/Application.java
+++ src/main/java/org/heimdall/shield_server/Application.java
@@ -1,8 +1,19 @@
 package org.heimdall.shield_server;
 
+import sun.misc.Signal;
+import sun.misc.SignalHandler;
+
 public class Application {
+
+    private static class ApplicationHandler implements SignalHandler {
+        public void handle(Signal signal) {
+            System.out.println("收到终止信号。");
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         new ShieldServer().start();
+        Signal.handle(new Signal("TERM"), new ApplicationHandler());
+        Signal.handle(new Signal("INT"), new ApplicationHandler());
     }
 }
