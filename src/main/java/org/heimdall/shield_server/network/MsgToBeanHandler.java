47e25f3d32a97c8c6fc6389413b298f889e2c53c
陈庆敬 <c.q.j@outlook.com>
2021-03-04 22:08:44 +0800

    增加入站消息ByteBuf引用计数检查

--- src/main/java/org/heimdall/shield_server/network/MsgToBeanHandler.java
+++ src/main/java/org/heimdall/shield_server/network/MsgToBeanHandler.java
@@ -5,10 +5,11 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.ReferenceCounted;
 import org.heimdall.shield_server.message.MsgBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-//import org.heimdall.shield_server.utils.LogUtil;
+
 import java.net.SocketAddress;
 
 /**
@@ -33,10 +34,19 @@ public class MsgToBeanHandler extends SimpleChannelInboundHandler<Object> {
         byte[] bytes = new byte[((ByteBuf)msg).readableBytes()];
         ((ByteBuf)msg).readBytes(bytes);
         if(isValidMessage(bytes)){
-            //channelHandlerContext.fireChannelRead(new MsgBean(bytes));
             MsgBean msgBean = new MsgBean(bytes);
         }else{
             logger.error("消息格式不合法，直接丢弃消息。消息长度{},魔数{}", bytes.length, (short)(((bytes[4] & 0xFF) << 8) | (bytes[5] & 0xFF)));
+        }
+        if(msg instanceof ReferenceCounted){
+            //检查ByteBuf的引用计数是否正常，避免内存泄漏
+            //ByteBuf的引用计数比较奇葩：刚创建出来的对象引用计数为2；调用retain引用计数增加 increment * 2；调用release引用计数减少 decrement * 2
+            //实际的引用计数计算公式：realCnt ：当原始引用计数为奇数，realCnt为0；当原始引用计数为偶数，realCnt = 原始引用计数 / 2；
+            //当decrement == realCnt时，意味着此时最后一个指向ByteBuf的引用被释放，此时置原始引用计数为1，也即realCnt为0，对象进入deallocate流程
+            //以上逻辑可参考：AbstractReferenceCountedByteBuf.java这个类,其refCnt字段即为原始引用计数
+            Integer referCount = ((ByteBuf)msg).refCnt();
+            //SimpleChannelInboundHandler稍后会自动释放一次引用,故此处值为1是正常的.
+            logger.info("当前消息是ByteBuf类型，当前引用计数{}，状态为{}", referCount, referCount == 1 ? "正常" : "异常");
         }
     }
 
